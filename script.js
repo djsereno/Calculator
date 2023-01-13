0ff9c574ad9ebcdc79e6c9214183767dfa5a347f
djsereno <djsereno91@gmail.com>
2023-01-13 11:57:55 -0800

    Add rolling calculations functionality

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let total = 0;
+let lastOperator = "add";
+let inputNextOperand = false;
 let display = {
   value: 0,
   docElement: document.querySelector(".display"),
@@ -9,20 +12,58 @@ let display = {
     let newValue = this.value.toString() + value;
     this.update(+newValue);
   },
+  clear: function () {
+    this.update(0);
+  },
 };
 display.update();
 
 numberButtons = document.querySelectorAll(".number");
 for (let i = 0; i < numberButtons.length; i++) {
   let number = numberButtons[i];
-  number.addEventListener("click", (e) => display.append(e.target.innerText));
+  number.addEventListener("click", (e) => {
+    // If the last operator pressed was "equals" and then a new
+    // number is input, then the user is starting a new equation
+    // and the old info should be cleared
+    if (lastOperator === "equals") allClear();
+
+    if (inputNextOperand) {
+      inputNextOperand = false;
+      display.update(e.target.innerText);
+    } else {
+      display.append(e.target.innerText);
+    }
+  });
 }
 
 clearButton = document.querySelector(".clear");
-clearButton.addEventListener("click", (e) => display.update(0));
+clearButton.addEventListener("click", allClear);
+
+operatorButtons = document.querySelectorAll(".operator");
+for (i = 0; i < operatorButtons.length; i++) {
+  let operatorButton = operatorButtons[i];
+  console.log(operatorButton);
+
+  operatorButton.addEventListener("click", (e) => {
+    // Every operator works as an equals button except that if the
+    // last operator was "equals" then there's no need to update "total"
+    if (lastOperator !== "equals") {
+      total = operate(lastOperator, total, display.value);
+    }
+    lastOperator = e.target.id;
+    display.update(total);
+    inputNextOperand = true;
+  });
+}
+
+function allClear() {
+  display.clear();
+  total = 0;
+  lastOperator = "add";
+}
 
 function add(x, y) {
-  return x + y;
+  return +x + +y;
 }
 
 function subtract(x, y) {
