be3b96cb1b6937f6929cc4cf69786581810f04ca
djsereno <djsereno91@gmail.com>
2023-01-13 18:26:41 -0800

    Add keyboard support

--- script.js
+++ script.js
@@ -51,53 +51,54 @@ let display = {
 };
 display.update();
 
+// KEYBOARD SUPPORT
+document.addEventListener("keydown", (e) => {
+  if (/[0-9.]/.test(e.key)) {
+    numberInput(e.key);
+    if (e.key === ".") decimalInput();
+  } else if (e.key === "Shift") {
+    display.changeSign();
+  } else if (e.key === "Backspace") {
+    display.delete();
+  } else if (e.key === "Delete") {
+    allClear();
+  } else if (e.key === "Enter" || /[\+\-\*\/]/.test(e.key)) {
+    let operation;
+    switch (e.key) {
+      case "+":
+        operation = "add";
+        break;
+      case "-":
+        operation = "subtract";
+        break;
+      case "*":
+        operation = "multiply";
+        break;
+      case "/":
+        operation = "divide";
+        break;
+      case "Enter":
+        operation = "equals";
+        break;
+    }
+    operatorInput(operation);
+  }
+});
+
 const numberButtons = document.querySelectorAll(".number");
 for (let i = 0; i < numberButtons.length; i++) {
   let number = numberButtons[i];
-  number.addEventListener("click", (e) => {
-    // If the last operator pressed was "equals" and then a new
-    // number is input, then the user is starting a new equation
-    // and the old info should be cleared
-    if (lastOperator === "equals" || typeof total === "string") allClear();
-    if (inputNextOperand) {
-      inputNextOperand = false;
-      display.clear();
-    }
-    if (e.target.id !== "decimal") {
-      display.append(e.target.innerText);
-      display.deletable = true;
-    }
-  });
+  number.addEventListener("click", (e) => numberInput(e.target.innerText));
 }
 
 const operatorButtons = document.querySelectorAll(".operator");
 for (i = 0; i < operatorButtons.length; i++) {
   let operatorButton = operatorButtons[i];
-
-  operatorButton.addEventListener("click", (e) => {
-    let lastOperatorButton = document.querySelector(".active");
-    if (lastOperatorButton) lastOperatorButton.classList.remove("active");
-    operatorButton.classList.add("active");
-
-    if (typeof total === "string") allClear();
-    // Every operator works as an equals button except that if the
-    // last operator was "equals" then there's no need to update "total"
-    if (lastOperator !== "equals") {
-      total = operate(lastOperator, total, display.value);
-    }
-    lastOperator = e.target.id;
-    typeof total === "string" ? display.draw(total) : display.update(total);
-    display.deletable = false;
-    inputNextOperand = true;
-  });
+  operatorButton.addEventListener("click", (e) => operatorInput(e.target.id));
 }
 
 const decimalButton = document.querySelector("#decimal");
-decimalButton.addEventListener("click", () => {
-  if (display.valueString.includes(".")) return;
-  display.valueString += ".";
-  display.draw();
-});
+decimalButton.addEventListener("click", decimalInput);
 
 const deleteButton = document.querySelector(".delete");
 deleteButton.addEventListener("click", () => display.delete());
@@ -107,6 +108,46 @@ clearButton.addEventListener("click", allClear);
 
 const changeSignButton = document.querySelector(".sign");
 changeSignButton.addEventListener("click", () => display.changeSign());
+
+function numberInput(number) {
+  // If the last operator pressed was "equals" and then a new
+  // number is input, then the user is starting a new equation
+  // and the old info should be cleared
+  if (lastOperator === "equals" || typeof total === "string") allClear();
+  if (inputNextOperand) {
+    inputNextOperand = false;
+    display.clear();
+  }
+  if (number !== ".") {
+    display.append(number);
+    display.deletable = true;
+  }
+}
+
+function operatorInput(operator) {
+  let lastOperatorButton = document.querySelector(".active");
+  if (lastOperatorButton) lastOperatorButton.classList.remove("active");
+
+  let operatorButton = document.querySelector("#" + operator);
+  operatorButton.classList.add("active");
+
+  if (typeof total === "string") allClear();
+  // Every operator works as an equals button except that if the
+  // last operator was "equals" then there's no need to update "total"
+  if (lastOperator !== "equals") {
+    total = operate(lastOperator, total, display.value);
+  }
+  lastOperator = operator;
+  typeof total === "string" ? display.draw(total) : display.update(total);
+  display.deletable = false;
+  inputNextOperand = true;
+}
+
+function decimalInput() {
+  if (display.valueString.includes(".")) return;
+  display.valueString += ".";
+  display.draw();
+}
 
 function allClear() {
   display.clear();
